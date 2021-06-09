13d923852098560f8a2c992472fb47100f5e1595
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-09 09:36:53 +0200

    Add components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './page/nav/nav.component';
+import { HomeComponent } from './page/home/home.component';
+import { UsersComponent } from './page/users/users.component';
+import { ForbiddenComponent } from './page/forbidden/forbidden.component';
+import { UserEditComponent } from './page/user-edit/user-edit.component';
+import { LoginComponent } from './page/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    UsersComponent,
+    ForbiddenComponent,
+    UserEditComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
