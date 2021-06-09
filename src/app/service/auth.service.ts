4dca9fc6f20405fe70da1aa5d62fa059c2cc2d64
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-09 11:39:31 +0200

    Add login to auth service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,7 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 import { User } from '../model/user';
 import { ConfigService } from './config.service';
+import { UserService } from './user.service';
+
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +15,53 @@ export class AuthService {
   loginUrl = `${this.config.apiUrl}login`;
   logoutUrl = `${this.config.apiUrl}logout`;
 
-  currentUserSubject: BehaviorSubject<User> = new BehaviorSubject<User>(new User());
+  currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
   lastToken: string = '';
+  storageName = 'currentUser';
 
-  get currentUserValue(): User {
+  get currentUserValue(): User | null {
     return this.currentUserSubject.value;
   }
 
-  constructor(private config: ConfigService) {}
+  constructor(
+    private config: ConfigService,
+    private http: HttpClient,
+    private userService: UserService,
+    private router: Router
+    ) {}
+
+  login(loginData: User): Observable<any> {
+    return this.http.post<{ accessToken: string }>(
+      this.loginUrl,
+      { email: loginData.email, password: loginData.password }
+    ).pipe(
+      switchMap( (response) => {
+        if (response.accessToken) {
+          this.lastToken = response.accessToken;
+          return this.userService.query(`email=${loginData.email}`)
+        }
+        return of(null);
+      })
+    ).pipe(
+      tap( user => {
+        if (!user) {
+          localStorage.removeItem(this.storageName);
+          this.currentUserSubject.next(null);
+        } else {
+          let userObj:User;
+          if (Array.isArray(user)) {
+            userObj = user[0];
+          } else {
+            userObj = user;
+          }
+
+          userObj.token = this.lastToken;
+          localStorage.setItem(this.storageName, JSON.stringify(userObj));
+          this.currentUserSubject.next(userObj);
+        }
+      })
+    )
+    
+  }
+  
 }
