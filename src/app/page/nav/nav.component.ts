a60e8865bfda8635dd3fecd1ab055004076f0edf
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-09 13:47:30 +0200

    Add nav component

--- src/app/page/nav/nav.component.ts
+++ src/app/page/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/service/auth.service';
 import { ConfigService } from 'src/app/service/config.service';
 
 @Component({
@@ -8,10 +11,20 @@ import { ConfigService } from 'src/app/service/config.service';
 })
 export class NavComponent implements OnInit {
   navigation = this.config.navigation;
+  loginStatus:boolean = false;
+  user: User | null = null;
+  userSub: Subscription | null = null;
 
-  constructor(private config: ConfigService) { }
+  constructor(private config: ConfigService, private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.auth.currentUserSubject.subscribe(
+      (user: User | null) => this.user = user 
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.userSub?.unsubscribe();
   }
 
 }
